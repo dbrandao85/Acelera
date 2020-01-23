eae036689b670026a9dbe6721cdabc34c4b4852c
Daniel Brandão <50630500+dbrandao85@users.noreply.github.com>
2020-01-23 17:30:02 -0300

    Json inicio

---
+++ AceleraDev/AceleraDev/Dados.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AceleraDev
+{
+    class Dados
+    {
+        [JsonPropertyName("Acelera")]
+        public string NumeroCasas { get; set; }
+        public string Token { get; set; }
+        public string Cifrado { get; set; }
+        public string Decifrado { get; set; }
+        public string ResumoCriptografico { get; set; }
+    }
+}
--- AceleraDev/AceleraDev/Form1.cs
+++ AceleraDev/AceleraDev/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,12 +19,36 @@ namespace AceleraDev
             InitializeComponent();
         }
 
+        Dados obj = new Dados();
+        
         private void button1_Click(object sender, EventArgs e)
         {
-
+            ReceberJson();
         }
 
         private void button2_Click(object sender, EventArgs e)
+        {
+            EnviarJson();
+        }
+
+        private void ReceberJson()
+        {
+            string responseString;
+
+            var webClient = new WebClient();
+            
+            responseString = webClient.DownloadString("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=1adcc80b66da14db6aaee5ee7f388e95e5d7827a");
+
+            var obj2= JsonSerializer.Deserialize<Dados>(responseString);
+
+            Console.WriteLine(responseString, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            }); 
+            textBox1.Text = obj2.Cifrado;
+        }
+
+        private void EnviarJson()
         {
 
         }
